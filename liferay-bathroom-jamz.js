e2cca58122ebfccc1dbc56648123fb57a1b80054
Weston Hancock <weston.hancock@liferay.com>
2016-01-27 09:01:37 -0800

    source formatting

--- liferay-bathroom-jamz.js
+++ liferay-bathroom-jamz.js
@@ -2,15 +2,15 @@ if (Meteor.isClient) {
 	function findWinner() {
 		var list = Music.find().fetch();
 		var highestVoteMusic = list[0];
-		
-		console.log("List of potential music votes");			
+
+		console.log("List of potential music votes");
 		for(var i=1; i < list.length; i++) {
 			console.log(list[i].name + " : " + list[i].votes);
 			if(highestVoteMusic.votes < list[i].votes) {
 				highestVoteMusic = list[i];
 			}
 		}
-		return highestVoteMusic._id;			
+		return highestVoteMusic._id;
 	}
 
 	function resetCollections() {
@@ -18,55 +18,57 @@ if (Meteor.isClient) {
 
 		music.forEach(function(item) {
 			var id = item._id;
-			
+
 			Music.update(id, {
 				$set: {votes: 0}
 			});
 		});
-		
+
 		CurrentSelection.remove({});
-	}	
-	
-	
+	}
+
+
 	onYouTubeIframeAPIReady = function() {
-      player = new YT.Player("player", {
+		player = new YT.Player(
+			"player",
+			{
+				height: "400",
+				width: "600",
+				videoId: "LdH1hSWGFGU",
+				events: {
+						'onReady': onPlayerReady
+				}
+			}
+		);
+	}
 
-          height: "400",
-          width: "600",
-
-          // videoId is the "v" in URL (ex: http://www.youtube.com/watch?v=LdH1hSWGFGU, videoId = "LdH1hSWGFGU")
-          videoId: "LdH1hSWGFGU",
-
-          // Events like ready, state change,
-          events: {
-              'onReady': onPlayerReady
-          }
-      });
-  }
-
-  function onPlayerReady(event) {
-	window.setInterval(function(event) {
+	function onPlayerReady(event) {
+		window.setInterval(function(event) {
 			var date = new Date();
 			var time = date.getSeconds();
 
-			if(time == 0 || time == 15 || time == 30 || time == 45) {	
+			if(time == 0 || time == 15 || time == 30 || time == 45) {
 				var _id = findWinner();
 
-				player.loadPlaylist({listType:"playlist",
-				  list: Music.findOne(_id).playerId,
-				  index:0,
-				  startSeconds:0,
-				  suggestedQuality:"large"})
+				player.loadPlaylist(
+					{
+						listType:"playlist",
+						list: Music.findOne(_id).playerId,
+						index:0,
+						startSeconds:0,
+						suggestedQuality:"large"
+					}
+				);
 
-				event.target.playVideo();		
+				event.target.playVideo();
 
 				resetCollections()
 			}
 		}, 1000);
-  }
+	}
 
-  YT.load();	
-  
+	YT.load();
+
 	//Login Templates -- DONT REMOVE
 	Template.body.helpers({
 		firstName: function(){
